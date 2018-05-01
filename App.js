d6fbf32c0f7f0388ed423052367cab9e8e85e44f
Perry Liao <perryliaohc@gmail.com>
2018-05-01 00:02:55 -0700

    added keyboard view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { styles } from './styles'
 
 export default class App extends React.Component {
@@ -47,8 +47,9 @@ export default class App extends React.Component {
     }
   }
 
-  confirmPassword(pw2) {
-      this.setState({ validConfirm: this.state.password === pw2});
+  confirmPassword(pw1, pw2) {
+    if (this.state.validPassword)
+      this.setState({ validConfirm: pw1 === pw2});
 }
 
   render() {
@@ -57,66 +58,69 @@ export default class App extends React.Component {
     const { validConfirm } = this.state;
 
       return (
-        <ScrollView>
-        <View style={{flex: 1}}>
-        <View style={styles.logo}>
-          <Image source={require('./resources/urEnvelope.png')} style={styles.logo} />
-        </View>
-        <Text style ={styles.title}> Create an account... </Text>
-        {/*
-          username
-          */}
-        <Text style ={styles.texts}> Username </Text>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Enter your username here!"
-            onChangeText={(text) => {
-              this.setState({username: text});
-              this.validateUsername(text);
-            }}
-            returnKeyType = "next"
-          />
-          { validUsername && <Text>valid username</Text> }
-          { !validUsername && <Text style={styles.errorMessage}>Usernames must be under 13 characters long.</Text> }
-          {/*
-            password
+        <KeyboardAvoidingView behavior="padding" enabled>
+          <ScrollView>
+            <View style={{flex: 1}}>
+              <View style={styles.logo}>
+                  <Image source={require('./resources/urEnvelope.png')} style={styles.logo} />
+              </View>
+            <Text style ={styles.title}> Create an account... </Text>
+            {/*
+              username
             */}
-          <Text style ={styles.texts}> Password </Text>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Enter your password here!"
-            onChangeText={(text) => {
-              this.setState({password: text});
-              this.validatePassword(text);
-            }}
-            returnKeyType = "next"
-            secureTextEntry = {true}
-          />
+            <Text style ={styles.texts}> Username </Text>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Enter your username here!"
+                onChangeText={(text) => {
+                  this.setState({username: text});
+                  this.validateUsername(text);
+                }}
+                returnKeyType = "next"
+                />
+                { validUsername && <Text style={styles.validMessage}>valid username</Text> }
+                { !validUsername && <Text style={styles.errorMessage}>Usernames must be under 13 characters long.</Text> }
+            {/*
+              password
+            */}
+            <Text style ={styles.texts}> Password </Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter your password here!"
+              onChangeText={(text) => {
+                this.setState({password: text});
+                this.validatePassword(text);
+                this.confirmPassword(text, this.state.password2)
+              }}
+              returnKeyType = "next"
+              secureTextEntry = {true}
+              />
 
-          { validPassword && <Text>valid password</Text> }
-          { !validPassword && <Text style={styles.errorMessage}>Enter a 6-16 digit password, containing at least 1 lower and upper case alphabet, 1 number and 1 symbol.</Text> }
-          {/*
-            confirm password
-           */}
-          <Text style ={styles.texts}> Confirm Password </Text>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Re-enter your password here!"
-            onChangeText={(text) => {
-              this.setState({password2: text});
-              this.confirmPassword(text);
-            }}
-            returnKeyType = "done"
-            secureTextEntry = {true}
-          />
-          { validConfirm && <Text>valid confirm</Text> }
-          { !validConfirm && <Text style={styles.errorMessage}>Passwords do not match! {this.state.password, this.state.password2, this.state.validConfirm}</Text> }
+              { validPassword && <Text style={styles.validMessage}>valid password</Text> }
+              { !validPassword && <Text style={styles.errorMessage}>Enter a 6-16 digit password, containing at least 1 lower and upper case alphabet, 1 number and 1 symbol.</Text> }
+              {/*
+                confirm password
+              */}
+              <Text style ={styles.texts}> Confirm Password </Text>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Re-enter your password here!"
+                onChangeText={(text) => {
+                  this.setState({password2: text});
+                  this.confirmPassword(this.state.password, text);
+                }}
+                returnKeyType = "done"
+                secureTextEntry = {true}
+                />
+                { validConfirm && <Text style={styles.validMessage}>valid confirm</Text> }
+                { !validConfirm && <Text style={styles.errorMessage}>Passwords do not match, or is invalid! {this.state.password, this.state.password2, this.state.validConfirm}</Text> }
 
-          <TouchableOpacity style ={styles.button}>
-            <Text style ={styles.buttonText}> CREATE ACCOUNT </Text>
-          </TouchableOpacity>
-        </View>
-        </ScrollView>
+                <TouchableOpacity style ={styles.button}>
+                  <Text style ={styles.buttonText}> CREATE ACCOUNT </Text>
+                </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
       );
     }
   }
